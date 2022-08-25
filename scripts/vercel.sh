c8c4027c1de33b7e651a8f9a823afcd081558f65
Jota <jotarjotinha@gmail.com>
2022-08-25 13:37:09 -0300

    remove elif

--- scripts/vercel.sh
+++ scripts/vercel.sh
@@ -4,10 +4,6 @@ if [[ "$VERCEL_GIT_COMMIT_REF" == "master" || "$VERCEL_GIT_COMMIT_REF" == "stagi
 then
   echo "Building..."
   exit 1;
-elif [[ "$VERCEL_GIT_COMMIT_MESSAGE" =~ "[nobuild]" ]]
-then
-  echo "Building..."
-  exit 0;
 else
   echo "Building cancelled."
   exit 0;
