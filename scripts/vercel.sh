640f3cc57df0645c81b946ea388b04e3321a71c3
Jota <jotarjotinha@gmail.com>
2022-08-25 13:53:54 -0300

    no more vercel comments and changed script once again

--- scripts/vercel.sh
+++ scripts/vercel.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "master" || "$VERCEL_GIT_COMMIT_REF" == "staging" ]]
+if [[ "$VERCEL_GIT_COMMIT_REF" != "master" || "$VERCEL_GIT_COMMIT_REF" != "staging" || "$VERCEL_GIT_COMMIT_MESSAGE" =~ "[nobuild]" ]]
 then
-  echo "Building..."
-  exit 1;
-else
   echo "Building cancelled."
   exit 0;
+else
+  echo "Building..."
+  exit 1;
 fi
